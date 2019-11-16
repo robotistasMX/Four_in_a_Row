581ef4cb81b765ccf0bacb456535113b7b833e94
afr2903 <afr102903@gmail.com>
2019-11-16 11:36:41 -0600

    Calibration test

---
+++ fichas.py
@@ -0,0 +1,51 @@
+import cv2 as cv
+import numpy as np
+cap= cv.VideoCapture(1)
+
+def nothing(x):
+    pass
+
+cv.namedWindow('slider')
+cv.createTrackbar('Low Hue', 'slider', 0, 255, nothing)
+cv.createTrackbar('Low Sat', 'slider', 0, 255, nothing)
+cv.createTrackbar('Low Val', 'slider', 0, 255, nothing)
+cv.createTrackbar('Upper Hue', 'slider', 0, 255, nothing)
+cv.createTrackbar('Upper Sat', 'slider', 0, 255, nothing)
+cv.createTrackbar('Upper Val', 'slider', 0, 255, nothing)
+
+while(1):
+    ret, frame= cap.read()
+    hsv= cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    lowHue = cv.getTrackbarPos('Low Hue','slider')
+    lowSat = cv.getTrackbarPos('Low Sat','slider')
+    lowVal = cv.getTrackbarPos('Low Val','slider')
+    upperHue = cv.getTrackbarPos('Upper Hue','slider')
+    upperSat = cv.getTrackbarPos('Upper Sat','slider')
+    upperVal = cv.getTrackbarPos('Upper Val','slider')
+
+    low= np.array([lowHue,lowSat,lowVal])
+    upper= np.array([upperHue,upperSat,upperVal])
+    filter=cv.inRange(hsv, low, upper)
+
+    filter = cv.GaussianBlur(filter, (1,1), 2)
+    filter = cv.medianBlur(filter,5)
+    filter = cv.dilate(filter, cv.getStructuringElement(cv.MORPH_RECT,(5,5)), iterations=1)
+    filter = cv.erode(filter, cv.getStructuringElement(cv.MORPH_RECT,(3,3)), iterations=1)
+    filter = cv.erode(filter, cv.getStructuringElement(cv.MORPH_RECT,(5,5)), iterations=1)
+
+    result = cv.bitwise_and(frame, frame, mask = filter)
+    object= cv.moments(filter)
+    if object['m00'] > 50000:
+        cx= int(object['m10']/object['m00'])
+        cy= int(object['m01']/object['m00'])
+        cv.circle(result, (cx,cy), 40, (0,255,0), 2)
+
+    cv.imshow('frame', frame)
+    cv.imshow('result', result)
+
+    w=cv.waitKey(1)
+    if w==27:
+        break
+
+cap.release()
+cv.destroyAllWindows
